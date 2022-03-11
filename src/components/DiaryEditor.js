07be799b11083b2cbb2877b51daf07538dc8cebd
leehyeeun <leehe0430@gmail.com>
2022-03-11 21:27:01 +0900

    2022/3/11 EmotionDiary Review Optimization

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import EmotionItem from "./EmotionItem";
 import Mybutton from "./MyButton";
